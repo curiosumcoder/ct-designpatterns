b17d70a2ee3019871a5656dad5ba6fa8a2da08f5
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-06-14 21:18:57 -0600

    Create IMaintenance.cs

---
+++ 20230614/wa7/Nothwind.Data/IMaintenance.cs
@@ -0,0 +1,17 @@
+﻿using Northwind.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nothwind.Data
+{
+    public interface IMaintenance<T>
+    {
+        void Create(T m);
+        void Delete(T m);
+        List<T> List();
+        void Update(T m);
+    }
+}
