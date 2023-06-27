634945dee1cde71b25894300221b5935204b59e5
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-06-26 21:58:40 -0600

    Update

---
+++ 20230626/wa9/wpf9/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using System.Windows;
+
+namespace wpf9.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string? _userName;
+
+        [ObservableProperty]
+        string? _password;
+
+        [RelayCommand()]
+        void Submit()
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password)) return;
+
+            MessageBox.Show($"Welcome {UserName}!");
+        }
+    }
+}
