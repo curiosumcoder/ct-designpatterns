26239fcca17d1788771d62a9d2bf7f7d7314915d
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-06-26 20:03:54 -0600

    Updates

---
+++ 20230626/wa9/Data/Numbers.cs
@@ -0,0 +1,22 @@
+﻿namespace Data
+{
+    public class Numbers : IGetList<int>
+    {
+        public IEnumerable<int> GetList()
+        {
+            //throw new NotImplementedException();
+
+            //return new int[] { };
+
+            for (int i = 0; i < 15; i++)
+            {
+                yield return new Random().Next(1, 1000);
+            }
+        }
+
+        public bool IsOdd(int valor)
+        {
+            return valor % 2 == 1;
+        }
+    }
+}
---
+++ 20230626/wa9/wa9/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using wa9.Models;
+
+namespace wa9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        readonly IGetList<int> _nums;
+
+        public HomeController(ILogger<HomeController> logger, IGetList<int> nums)
+        {
+            _logger = logger;
+            _nums = nums;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_nums.GetList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
